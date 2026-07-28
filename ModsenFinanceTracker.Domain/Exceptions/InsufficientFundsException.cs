12cb5022995944e12f7966a4aa30acfe7bbbded5
notawhle <notawh4le@gmail.com>
2026-07-28 13:08:34 +0300

    feat(domain): add domain events and custom exceptions

---
+++ ModsenFinanceTracker.Domain/Exceptions/InsufficientFundsException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModsenFinanceTracker.Domain.Exceptions
+{
+    public class InsufficientFundsException : Exception
+    {
+        public decimal CurrentBalance { get; }
+        public decimal RequestedAmount { get; }
+        public InsufficientFundsException(decimal currentBalance, decimal requestedAmount)
+            : base($"Insufficient funds. Current balance: {currentBalance}, requested amount: {requestedAmount}")
+        {
+            CurrentBalance = currentBalance;
+            RequestedAmount = requestedAmount;
+        }
+    }
+}
