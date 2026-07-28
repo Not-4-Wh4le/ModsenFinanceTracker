5034164342e4939998a7efd0949c0de999d8e376
notawhle <notawh4le@gmail.com>
2026-07-28 13:09:40 +0300

    feat(domain): add AppConfiguration class

---
+++ ModsenFinanceTracker.Domain/Entities/AppConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModsenFinanceTracker.Domain.Entities
+{
+    public class AppConfiguration
+    {
+        public string Currency { get; set; } 
+        public string DataFilePath { get; set; }
+        public string DateFormat { get; set; }
+
+        public AppConfiguration(string currency, string dataFilePath, string dateFormat)
+        {
+            Currency = string.IsNullOrEmpty(currency) ? currency : "";
+            DataFilePath = string.IsNullOrEmpty(dataFilePath) ? dataFilePath : "data.json";
+            DateFormat = string.IsNullOrEmpty(dateFormat) ? dateFormat : "dd.MM.yyyy";
+        }
+    }
+}
