0e9590c8df9878d1b5fd98625186067ade6f78f7
notawhle <notawh4le@gmail.com>
2026-07-28 13:05:33 +0300

    feat(domain): add IEntity and IDomainEvent interfaces, AggregateRoot abstract class

---
+++ ModsenFinanceTracker.Domain/Common/AggregateRoot.cs
@@ -0,0 +1,20 @@
+﻿using ModsenFinanceTracker.Domain.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModsenFinanceTracker.Domain.Common
+{
+    public abstract class AggregateRoot
+    {
+        private List<IDomainEvent> _events = new();
+        public IReadOnlyCollection<IDomainEvent> Events => _events.AsReadOnly();
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _events.Add(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+            => _events.Clear();
+    }
+}
