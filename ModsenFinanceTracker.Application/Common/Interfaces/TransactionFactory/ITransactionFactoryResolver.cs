8b99eb323d254d6461822abb1eb4b2f9e3c320b5
notawhle <notawh4le@gmail.com>
2026-07-30 12:49:06 +0300

    feat(application): add transaction factory and factory resolver

---
+++ ModsenFinanceTracker.Application/Common/Interfaces/TransactionFactory/ITransactionFactoryResolver.cs
@@ -0,0 +1,11 @@
+﻿using ModsenFinanceTracker.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModsenFinanceTracker.Application.Common.Interfaces.TransactionFactory;
+
+internal interface ITransactionFactoryResolver
+{
+    ITransactionFactory GetFactory(TransactionType transactionType);
+}
---
+++ ModsenFinanceTracker.Application/Common/TransactionFactoryImpl/TransactionFactoryResolver.cs
@@ -0,0 +1,22 @@
+﻿using ModsenFinanceTracker.Application.Common.Interfaces.TransactionFactory;
+using ModsenFinanceTracker.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModsenFinanceTracker.Application.Common.TransactionFactoryImpl;
+
+public class TransactionFactoryResolver : ITransactionFactoryResolver
+{
+    private readonly IReadOnlyDictionary<TransactionType, ITransactionFactory> _transactionFactories;
+
+    public TransactionFactoryResolver(IEnumerable<ITransactionFactory> transactionFactories)
+    {
+        _transactionFactories = transactionFactories.ToDictionary(f => f.Type);
+    }
+
+    public ITransactionFactory GetFactory(TransactionType transactionType)
+    {
+        return _transactionFactories[transactionType];
+    }
+}
