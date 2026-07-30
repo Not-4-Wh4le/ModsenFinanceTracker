da8129e3cf889e3ebac26cfbef92426d27514181
notawhle <notawh4le@gmail.com>
2026-07-30 12:49:58 +0300

    feat(application): register repositories and transaction factories in DI

---
+++ ModsenFinanceTracker.Application/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ModsenFinanceTracker.Application.Common.Interfaces.TransactionFactory;
+using ModsenFinanceTracker.Application.Common.TransactionFactoryImpl;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ModsenFinanceTracker.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        services.AddSingleton<ITransactionFactory, IncomeTransactionFactory>();
+        services.AddSingleton<ITransactionFactory, ExpenseTransactionFactory>();
+        services.AddSingleton<ITransactionFactoryResolver, TransactionFactoryResolver>();
+
+        services.AddMediatR(conf =>
+        {
+            conf.RegisterServicesFromAssemblies(assembly);
+        });
+
+        return services;
+    }
+}
