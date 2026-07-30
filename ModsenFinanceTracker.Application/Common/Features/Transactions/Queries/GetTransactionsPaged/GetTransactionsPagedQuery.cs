8492c68a58c409d5dff401ef5b5aadbe2041ffec
notawhle <notawh4le@gmail.com>
2026-07-30 13:44:44 +0300

    feat(application): add create and get paged transaction handlers

---
+++ ModsenFinanceTracker.Application/Common/Features/Transactions/Queries/GetTransactionsPaged/GetTransactionsPagedQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using ModsenFinanceTracker.Application.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModsenFinanceTracker.Application.Common.Features.Transactions.Queries.GetTransactionsPaged;
+
+public record GetTransactionsPagedQuery(
+    DateTime? StartDate = null,
+    DateTime? EndDate = null,
+    int PageNumber = 1,
+    int PageSize = 10)
+    : IRequest<PagedResultDto<TransactionDto>>;
