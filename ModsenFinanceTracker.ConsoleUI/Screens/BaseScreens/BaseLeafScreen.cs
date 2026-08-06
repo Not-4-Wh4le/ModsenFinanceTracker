57e472118215e2eb40285b1e23ffded2d04bfa6d
notawhle <notawh4le@gmail.com>
2026-08-06 16:06:19 +0300

    feat(ui): add base screens

---
+++ ModsenFinanceTracker.ConsoleUI/Screens/BaseScreens/BaseLeafScreen.cs
@@ -0,0 +1,31 @@
+﻿using Spectre.Console;
+
+namespace ModsenFinanceTracker.ConsoleUI.Screens.BaseScreens;
+
+public abstract class BaseLeafScreen
+{
+    protected abstract string GetHeader();
+    protected abstract Task HandleAsync();
+
+    public async Task ShowAsync()
+    {
+        Console.Clear();
+        AnsiConsole.Write(new Rule(GetHeader()));
+        try
+        {
+            await HandleAsync();
+        }
+        catch (Exception ex)
+        {
+            AnsiConsole.MarkupLine($"\n[red]Критическая ошибка:[/] {ex.Message}");
+        }
+        await WaitForKeyPressAsync();
+    }
+
+    private static async Task WaitForKeyPressAsync()
+    {
+        AnsiConsole.WriteLine();
+        AnsiConsole.MarkupLine("[grey]Нажмите любую клавишу для возврата[/]");
+        await Task.Run(() => Console.ReadKey(true));
+    }
+}
---
+++ ModsenFinanceTracker.ConsoleUI/Screens/BaseScreens/BaseMenuScreen.cs
@@ -0,0 +1,47 @@
+﻿using Spectre.Console;
+
+namespace ModsenFinanceTracker.ConsoleUI.Screens.BaseScreens;
+
+public abstract class BaseMenuScreen
+{
+    protected abstract string GetHeader();
+    protected abstract List<(string Title, Func<Task> Action)> ConfMenu();
+    protected virtual string ExitOptionText => "Назад";
+    protected virtual bool ShouldClose() => false;
+    protected virtual Task OnExitAsync() => Task.CompletedTask;
+
+    public virtual async Task ShowAsync()
+    {
+        bool isInside = true;
+
+        while (isInside)
+        {
+            Console.Clear();
+            AnsiConsole.Write(new Rule(GetHeader()));
+
+            var menuItems = ConfMenu();
+
+            var selectionPrompt = new SelectionPrompt<string>()
+                .AddChoices(menuItems.Select(i => i.Title));
+
+            selectionPrompt.AddChoice(ExitOptionText);
+
+            var choice = AnsiConsole.Prompt(selectionPrompt);
+
+            if (choice == ExitOptionText)
+            {
+                isInside = false;
+                await OnExitAsync();
+                continue;
+            }
+
+            var selectedAction = menuItems.First(i => i.Title == choice).Action;
+            await selectedAction();
+
+            if (ShouldClose())
+            {
+                isInside = false;
+            }
+        }
+    }
+}
