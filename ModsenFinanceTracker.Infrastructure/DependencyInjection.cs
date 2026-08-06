35799fcd5d92a91486a88c16c5433de37b5b3aa8
notawhle <notawh4le@gmail.com>
2026-08-06 12:16:06 +0300

    feat(infrastructure): add data seeder and di

---
+++ ModsenFinanceTracker.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ModsenFinanceTracker.Application.Common.Interfaces.Repositories;
+using ModsenFinanceTracker.Infrastructure.InMemoryStorage;
+using ModsenFinanceTracker.Infrastructure.InMemoryStorage.Repositories;
+
+namespace ModsenFinanceTracker.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddSingleton<InMemoryDbContext>();
+
+        services.AddSingleton<IWalletRepository, InMemoryWalletRepository>();
+        services.AddSingleton<ICategoryRepository, InMemoryCategoryRepository>();
+        services.AddSingleton<ITransactionRepository, InMemoryTransactionRepository>();
+
+        return services;
+    }
+}
