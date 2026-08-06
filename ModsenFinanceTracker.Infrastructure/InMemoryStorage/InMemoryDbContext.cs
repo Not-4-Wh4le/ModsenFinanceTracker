c4e383e12f8fce8fcb048991172046421bbbb3c4
notawhle <notawh4le@gmail.com>
2026-08-06 11:53:47 +0300

    feat(infrastructure): add in-memory repository implementations

---
+++ ModsenFinanceTracker.Infrastructure/InMemoryStorage/InMemoryDbContext.cs
@@ -0,0 +1,9 @@
+﻿using ModsenFinanceTracker.Domain.Entities;
+
+namespace ModsenFinanceTracker.Infrastructure.InMemoryStorage;
+
+public class InMemoryDbContext
+{
+    public List<Wallet> Wallets { get; } = new();
+    public List<Category> Categories { get; } = new();
+}
