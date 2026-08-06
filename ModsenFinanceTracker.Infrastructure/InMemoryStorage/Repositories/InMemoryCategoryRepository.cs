e1e1acdea4e574f4cc24eb042b79f78522b345d1
notawhle <notawh4le@gmail.com>
2026-08-06 11:53:47 +0300

    feat(infrastructure): add in-memory repository implementations

---
+++ ModsenFinanceTracker.Infrastructure/InMemoryStorage/Repositories/InMemoryCategoryRepository.cs
@@ -0,0 +1,51 @@
+﻿using ModsenFinanceTracker.Application.Common.Interfaces.Repositories;
+using ModsenFinanceTracker.Domain.Entities;
+
+namespace ModsenFinanceTracker.Infrastructure.InMemoryStorage.Repositories;
+
+public class InMemoryCategoryRepository : ICategoryRepository
+{
+
+    private readonly InMemoryDbContext _context;
+
+    public InMemoryCategoryRepository(InMemoryDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        _context.Categories.RemoveAll(c => c.Id == id);
+        
+        return Task.CompletedTask;
+    }
+
+    public Task<Category?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+        return Task.FromResult(category);
+    }
+
+    public Task<(IReadOnlyCollection<Category>, int TotalCount)> GetPagedAsync(int pageNumber, int pageSize, CancellationToken cancellationToken = default)
+    {
+        var categories = _context.Categories
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        var totalCount = _context.Categories.Count;
+        
+        return Task.FromResult(((IReadOnlyCollection<Category>)categories, totalCount));
+    }
+
+    public Task SaveAsync(Category category, CancellationToken cancellationToken = default)
+    {
+        if (!_context.Categories.Any(c => c.Id == category.Id))
+        {
+            _context.Categories.Add(category);
+        }
+
+        return Task.CompletedTask;
+    }
+}
