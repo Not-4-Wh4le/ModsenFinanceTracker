a34b62e83e11fc03feaadd86ba27c383f7370173
notawhale <notawh4le@gmail.com>
2026-08-06 16:43:59 +0300

    feat(application): add Create and GetPaged category handlers

---
+++ ModsenFinanceTracker.Application/Common/Features/Categories/Commands/CreateCategory/CreateCategoryCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using ModsenFinanceTracker.Application.Common.Interfaces.Repositories;
+using ModsenFinanceTracker.Domain.Entities;
+
+namespace ModsenFinanceTracker.Application.Common.Features.Categories.Commands.CreateCategory;
+
+public class CreateCategoryCommandHandler
+    : IRequestHandler<CreateCategoryCommand, Guid>
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CreateCategoryCommandHandler(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<Guid> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = new Category(
+            Guid.NewGuid(),
+            request.Name,
+            request.TransactionType,
+            request.BudgetLimit);
+
+        await _categoryRepository.SaveAsync(category, cancellationToken);
+        return category.Id;
+    }
+}
---
+++ ModsenFinanceTracker.Application/Common/Features/Categories/Queries/GetCategoriesPaged/GetCategoriesPagedQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using ModsenFinanceTracker.Application.Common.Models;
+using ModsenFinanceTracker.Domain.Enums;
+
+namespace ModsenFinanceTracker.Application.Common.Features.Categories.Queries.GetCategoriesPaged;
+
+public record GetCategoriesPagedQuery(
+    TransactionType? TransactionType,
+    int PageNumber = 1,
+    int PageSize = 10) 
+    : IRequest<PagedResultDto<CategoryDto>>;
