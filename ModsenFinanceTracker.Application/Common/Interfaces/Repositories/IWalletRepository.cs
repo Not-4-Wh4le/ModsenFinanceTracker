84fdd0d53370f347a78221044a544096cc3a9e87
notawhale <notawh4le@gmail.com>
2026-08-10 09:30:27 +0300

    feat(application): add GetPagedAsync method to wallet repository

--- ModsenFinanceTracker.Application/Common/Interfaces/Repositories/IWalletRepository.cs
+++ ModsenFinanceTracker.Application/Common/Interfaces/Repositories/IWalletRepository.cs
@@ -5,6 +5,10 @@ namespace ModsenFinanceTracker.Application.Common.Interfaces.Repositories
     public interface IWalletRepository
     {
         Task<Wallet?> GetAsync(Guid Id, CancellationToken cancellationToken = default);
+        Task<(IReadOnlyCollection<Wallet>, int TotalCount)> GetPagedAsync(
+            int pageNumber,
+            int pageSize,
+            CancellationToken cancellationToken = default);
         Task SaveAsync(Wallet wallet, CancellationToken cancellationToken = default);
     }
 }
