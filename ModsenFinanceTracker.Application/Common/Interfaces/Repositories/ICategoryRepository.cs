cbd2e5e8c75db1dbe80f88aad0bea53d60181fb5
notawhle <notawh4le@gmail.com>
2026-08-13 11:15:05 +0300

    feat(application): add transaction type filter parametr to category repository

--- ModsenFinanceTracker.Application/Common/Interfaces/Repositories/ICategoryRepository.cs
+++ ModsenFinanceTracker.Application/Common/Interfaces/Repositories/ICategoryRepository.cs
@@ -1,10 +1,12 @@
 ﻿using ModsenFinanceTracker.Domain.Entities;
+using ModsenFinanceTracker.Domain.Enums;
 
 namespace ModsenFinanceTracker.Application.Common.Interfaces.Repositories
 {
     public interface ICategoryRepository
     {
         Task<(IReadOnlyCollection<Category>, int TotalCount)> GetPagedAsync(
+            TransactionType? transactionType,
             int pageNumber,
             int pageSize,
             CancellationToken cancellationToken = default);
