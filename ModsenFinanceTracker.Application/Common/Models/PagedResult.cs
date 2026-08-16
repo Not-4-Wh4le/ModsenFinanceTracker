ca6503b2150948f0c64c449490dd7d099dcaa853
notawhale <notawh4le@gmail.com>
2026-08-16 23:40:33 +0300

    style(application): remove unused usings

--- ModsenFinanceTracker.Application/Common/Models/PagedResult.cs
+++ ModsenFinanceTracker.Application/Common/Models/PagedResult.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ModsenFinanceTracker.Application.Common.Models;
+﻿namespace ModsenFinanceTracker.Application.Common.Models;
 
 public record PagedResult<T>(
         IReadOnlyList<T> Items,
