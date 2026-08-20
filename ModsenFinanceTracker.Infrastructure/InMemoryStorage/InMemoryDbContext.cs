c77ffb4f9a7417f0d9eb889aef572131fa445732
notawhale <notawh4le@gmail.com>
2026-08-20 14:06:51 +0300

    refactor(infrastructure): remove unused in-memory storage

--- ModsenFinanceTracker.Infrastructure/InMemoryStorage/InMemoryDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using ModsenFinanceTracker.Domain.Entities;
-
-namespace ModsenFinanceTracker.Infrastructure.InMemoryStorage;
-
-public class InMemoryDbContext
-{
-    public List<Wallet> Wallets { get; } = new();
-    public List<Category> Categories { get; } = new();
-}
