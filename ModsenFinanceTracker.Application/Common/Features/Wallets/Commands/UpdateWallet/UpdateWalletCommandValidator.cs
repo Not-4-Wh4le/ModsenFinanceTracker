42a58b0376d162d38d57da369374506c42fc36b3
notawhale <notawh4le@gmail.com>
2026-08-27 10:16:39 +0300

    feat(validation): add validation of commands and queries for wallets

---
+++ ModsenFinanceTracker.Application/Common/Features/Wallets/Commands/UpdateWallet/UpdateWalletCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using ModsenFinanceTracker.Domain.Entities;
+
+namespace ModsenFinanceTracker.Application.Common.Features.Wallets.Commands.UpdateWallet;
+
+public class UpdateWalletCommandValidator 
+    : AbstractValidator<UpdateWalletCommand>
+{
+    public UpdateWalletCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("Wallet ID is required");
+
+        RuleFor(x => x.NewName)
+            .NotEmpty()
+            .WithMessage("Name cannot be null or empty")
+            .MaximumLength(Wallet.MaxNameLength)
+            .WithMessage($"Length cannot be longer than {Wallet.MaxNameLength} characters");
+    }
+}
