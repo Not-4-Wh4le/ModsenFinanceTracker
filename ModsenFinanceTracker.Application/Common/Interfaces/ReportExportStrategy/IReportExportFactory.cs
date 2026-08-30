9fa31ae314af4af4b4bb8e14f36dbff1d3f07f44
notawhale <notawh4le@gmail.com>
2026-08-31 00:15:30 +0300

    feat(export): add export strategy and export factory interfaces

---
+++ ModsenFinanceTracker.Application/Common/Interfaces/ReportExportStrategy/IReportExportFactory.cs
@@ -0,0 +1,6 @@
+﻿namespace ModsenFinanceTracker.Application.Common.Interfaces.ReportExportStrategy;
+
+public interface IReportExportFactory
+{
+    IReportExportStrategy GetStrategy(string format);
+}
