ec9784ec235267c9dca660f3b3fe02fc97c5fd7b
notawhale <notawh4le@gmail.com>
2026-08-31 00:24:54 +0300

    feat(export): register factory and strategy in di

--- ModsenFinanceTracker.Application/DependencyInjection.cs
+++ ModsenFinanceTracker.Application/DependencyInjection.cs
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 ﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using ModsenFinanceTracker.Application.Common.Behavior;
+=======
+﻿using Microsoft.Extensions.DependencyInjection;
+using ModsenFinanceTracker.Application.Common.Export;
+using ModsenFinanceTracker.Application.Common.Interfaces.ReportExportStrategy;
+>>>>>>> 3170751 (feat(export): register factory and strategy in di)
 using ModsenFinanceTracker.Application.Common.Interfaces.TransactionFactory;
 using ModsenFinanceTracker.Application.Common.TransactionFactoryImpl;
 using System.Reflection;
@@ -16,6 +22,10 @@ public static class DependencyInjection
         services.AddSingleton<ITransactionFactory, IncomeTransactionFactory>();
         services.AddSingleton<ITransactionFactory, ExpenseTransactionFactory>();
         services.AddSingleton<ITransactionFactoryResolver, TransactionFactoryResolver>();
+
+        services.AddSingleton<IReportExportStrategy, CsvReportExportStrategy>();
+        services.AddSingleton<IReportExportStrategy, TxtReportExportStrategy>();
+        services.AddSingleton<IReportExportFactory, ReportExportFactory>();
 
         services.AddMediatR(conf =>
         {
