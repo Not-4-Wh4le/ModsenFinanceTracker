e9807d4c31a5009ec426e331c912d8492f473dd8
notawhale <notawh4le@gmail.com>
2026-09-02 11:18:54 +0300

    chore: resolve rebase conflict

--- ModsenFinanceTracker.Application/DependencyInjection.cs
+++ ModsenFinanceTracker.Application/DependencyInjection.cs
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 ﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using ModsenFinanceTracker.Application.Common.Behavior;
-=======
-﻿using Microsoft.Extensions.DependencyInjection;
 using ModsenFinanceTracker.Application.Common.Export;
 using ModsenFinanceTracker.Application.Common.Interfaces.ReportExportStrategy;
->>>>>>> 3170751 (feat(export): register factory and strategy in di)
+using ModsenFinanceTracker.Application.Common.Behavior;
 using ModsenFinanceTracker.Application.Common.Interfaces.TransactionFactory;
 using ModsenFinanceTracker.Application.Common.TransactionFactoryImpl;
 using System.Reflection;
