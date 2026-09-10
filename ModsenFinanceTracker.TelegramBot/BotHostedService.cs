925566d049853e669de975b63dfe8d963961702f
notawhale <notawh4le@gmail.com>
2026-09-10 09:10:14 +0300

    feat(bot): setup hosted service, app configuration and DI

---
+++ ModsenFinanceTracker.TelegramBot/BotHostedService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace ModsenFinanceTracker.TelegramBot;
+
+public class BotHostedService : BackgroundService
+{
+    private readonly ITelegramBotClient _botClient;
+    private readonly IUpdateHandler _updateHandler;
+    private readonly ILogger<BotHostedService> _logger;
+
+    public BotHostedService(
+        ITelegramBotClient botClient,
+        IUpdateHandler updateHandler,
+        ILogger<BotHostedService> logger)
+    {
+        _botClient = botClient;
+        _updateHandler = updateHandler;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = [UpdateType.Message, UpdateType.CallbackQuery],
+            DropPendingUpdates = true 
+        };
+
+        User me = await _botClient.GetMe(stoppingToken);
+        _logger.LogInformation("{FirstName} (@{Username}) running", me.FirstName, me.Username);
+
+        await _botClient.ReceiveAsync(
+            updateHandler: _updateHandler,
+            receiverOptions: receiverOptions,
+            cancellationToken: stoppingToken
+        );
+    }
+}
--- ModsenFinanceTracker.TelegramBot/Program.cs
+++ ModsenFinanceTracker.TelegramBot/Program.cs
@@ -1,7 +1,47 @@
+using ModsenFinanceTracker.Application;
+using ModsenFinanceTracker.Domain.Configutaion;
+using ModsenFinanceTracker.Infrastructure;
+using ModsenFinanceTracker.Infrastructure.JsonStorage;
 using ModsenFinanceTracker.TelegramBot;
+using ModsenFinanceTracker.TelegramBot.Commands;
+using ModsenFinanceTracker.TelegramBot.States;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+string botToken = builder.Configuration["TelegramBot:Token"]
+    ?? throw new InvalidOperationException("TelegramBot:Token not specified");
+
+builder.Services.AddSingleton<ITelegramBotClient>(
+    sp => new TelegramBotClient(botToken));
+
+builder.Services.AddSingleton(sp => new AppConfiguration("BYN", "data.json", "dd.MM.yyyy"));
+
+var handlerType = typeof(IBotCommandHandler);
+var handlers = typeof(Program).Assembly
+    .GetTypes()
+    .Where(t => handlerType.IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+
+foreach (var handler in handlers)
+{
+    builder.Services.AddTransient(handlerType, handler);
+}
+
+builder.Services.AddSingleton<TransactionDraft>();
+builder.Services.AddSingleton<IUpdateHandler, UpdateHandler>();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+
+builder.Services.AddHostedService<BotHostedService>();
 
 var host = builder.Build();
+
+using (var scope = host.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<JsonDbContext>();
+    await dbContext.LoadAsync();
+}
+
 host.Run();
