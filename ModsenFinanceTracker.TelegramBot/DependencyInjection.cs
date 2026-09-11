e08a5271431f3d1ebdb77072d20ee37ae4886a8f
notawhale <notawh4le@gmail.com>
2026-09-11 15:09:11 +0300

    refactor(bot): extract telegram bot di registration into extension method

---
+++ ModsenFinanceTracker.TelegramBot/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using ModsenFinanceTracker.Domain.Configutaion;
+using ModsenFinanceTracker.TelegramBot.Commands;
+using ModsenFinanceTracker.TelegramBot.States;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+
+namespace ModsenFinanceTracker.TelegramBot;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddTelegramBot(this IServiceCollection services, IConfiguration configuration)
+    {
+        string botToken = configuration["TelegramBot:Token"]
+            ?? throw new InvalidOperationException("TelegramBot:Token not specified");
+
+        services.AddSingleton<ITelegramBotClient>(
+            sp => new TelegramBotClient(botToken));
+
+        services.AddSingleton(sp => new AppConfiguration("BYN", "data.json", "dd.MM.yyyy"));
+
+        var handlerType = typeof(IBotEndpoint);
+        var handlers = typeof(Program).Assembly
+            .GetTypes()
+            .Where(t => handlerType.IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+
+        foreach (var handler in handlers)
+        {
+            services.AddTransient(handlerType, handler);
+        }
+
+        services.AddSingleton<TransactionDraft>();
+        services.AddSingleton<IUpdateHandler, UpdateHandler>();
+
+        return services;
+    }
+}
--- ModsenFinanceTracker.TelegramBot/Program.cs
+++ ModsenFinanceTracker.TelegramBot/Program.cs
@@ -1,36 +1,12 @@
 using ModsenFinanceTracker.Application;
-using ModsenFinanceTracker.Domain.Configutaion;
 using ModsenFinanceTracker.Infrastructure;
 using ModsenFinanceTracker.Infrastructure.JsonStorage;
 using ModsenFinanceTracker.TelegramBot;
-using ModsenFinanceTracker.TelegramBot.Commands;
-using ModsenFinanceTracker.TelegramBot.States;
-using Telegram.Bot;
-using Telegram.Bot.Polling;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-string botToken = builder.Configuration["TelegramBot:Token"]
-    ?? throw new InvalidOperationException("TelegramBot:Token not specified");
 
-builder.Services.AddSingleton<ITelegramBotClient>(
-    sp => new TelegramBotClient(botToken));
-
-builder.Services.AddSingleton(sp => new AppConfiguration("BYN", "data.json", "dd.MM.yyyy"));
-
-var handlerType = typeof(IBotEndpoint);
-var handlers = typeof(Program).Assembly
-    .GetTypes()
-    .Where(t => handlerType.IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
-
-foreach (var handler in handlers)
-{
-    builder.Services.AddTransient(handlerType, handler);
-}
-
-builder.Services.AddSingleton<TransactionDraft>();
-builder.Services.AddSingleton<IUpdateHandler, UpdateHandler>();
-
+builder.Services.AddTelegramBot(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
 
