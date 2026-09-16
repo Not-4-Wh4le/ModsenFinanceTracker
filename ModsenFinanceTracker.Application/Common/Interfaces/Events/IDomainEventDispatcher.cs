4ac4786784395a4534568dd7503794c468827856
notawhale <notawh4le@gmail.com>
2026-09-16 11:14:41 +0300

    feat(budget-limit): add domain event dispatcher

---
+++ ModsenFinanceTracker.Application/Common/Interfaces/Events/IDomainEventDispatcher.cs
@@ -0,0 +1,8 @@
+﻿using ModsenFinanceTracker.Domain.Common;
+
+namespace ModsenFinanceTracker.Application.Common.Interfaces.Events;
+
+public interface IDomainEventDispatcher
+{
+    Task DispatchAndClearAsync(AggregateRoot root, CancellationToken cancellationToken = default);
+}
