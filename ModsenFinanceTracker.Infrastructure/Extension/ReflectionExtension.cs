2698e290b8ee55c83e51f94732f6f0affce4983d
notawhale <notawh4le@gmail.com>
2026-09-16 11:23:15 +0300

    refactor(budget-limit): switch restoration of aggregates to reflection

---
+++ ModsenFinanceTracker.Infrastructure/Extension/ReflectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ModsenFinanceTracker.Infrastructure.Extension;
+
+public static class ReflectionExtension
+{
+    public static void SetPrivateField<TEntity, TValue>(
+        this TEntity entity, 
+        string fieldName,
+        TValue value)
+    {
+        var type = typeof(TEntity);
+
+        var field = type.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance)
+            ?? throw new InvalidOperationException($"Field {fieldName} not found");
+
+        if (!field.FieldType.IsAssignableFrom(typeof(TValue)))
+        {
+            throw new ArgumentException();
+        }
+
+        field.SetValue(entity, value);
+    }
+}
